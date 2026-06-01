de712b8c3ab3880aafe3fee4d410407df8768d51
David Martinez <dmaparicio00@gmail.com>
2026-06-01 19:43:31 +0200

    resolucion de ejercicios

---
+++ ejercicios-basicos-avanzados/ejercicio14.js
@@ -0,0 +1,38 @@
+/*Contador de repeticiones: Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
+
+Por ejemplo, que devuelva: code:4, repeat: 1, eat: 1,...
+
+Podéis conformar el formato que queráis para la devolución del resultado siempre y cuando sea claro.
+
+Puedes usar este array para probar tu función:*/
+
+const words = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+];
+function repeatCounter(list) {
+  const counterObject = {};
+
+  for (let i = 0; i < list.length; i++) {
+    const currentWord = list[i];
+
+    if (counterObject[currentWord]) {
+      counterObject[currentWord]++;
+    } else {
+      counterObject[currentWord] = 1;
+    }
+  }
+
+  return counterObject;
+}
+
+console.log(repeatCounter(words));
